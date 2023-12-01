d0b6384ea2390523da441855a6df10c3fd32289d
Davichobits <davidruizdeveloper@gmail.com>
2023-12-01 11:48:59 -0500

    feact(Header) - Funcionalides de filtro y sort

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,4 @@
-import { getFields } from '../../utils/functions.js';
+import { getFields, filterByField, sortData, findElements } from '../../utils/functions.js';
 import { updateCardsContainer } from '../../utils/dom.js';
 
 export const Header = (data, cardsContainer) => {
@@ -21,15 +21,35 @@ export const Header = (data, cardsContainer) => {
     </nav>
   `;
 
+  // Dibujo la lista de campos
   headerEl.querySelector('#main-field').innerHTML = `
     <option value="">Todas las áreas</option>
     ${fields.map(field => `<option value="${field}">${field}</option>`)}
   `;
 
   let dataFiltered = [...data];
+  let elementsSearched = [...data];
+
+  // Evento para buscar
   headerEl.querySelector('#search').addEventListener('input', (event) => {
     const userInput = event.target.value;
-    dataFiltered = data.filter(element => element.name.toLowerCase().includes(userInput.toLowerCase()));
+    elementsSearched = findElements(dataFiltered, userInput);
+    updateCardsContainer(cardsContainer, elementsSearched);
+  });
+
+  // Evento para ordenar
+  headerEl.querySelector('#sort').addEventListener('change', (event) => {
+    const sortValue = event.target.value;
+    const dataSorted = sortData(dataFiltered, sortValue);
+    updateCardsContainer(cardsContainer, dataSorted);
+  });
+
+  // Evento para filtrar por campo
+  const selectField = headerEl.querySelector('#main-field')
+
+  selectField.addEventListener('change', (event) => {
+    const field = event.target.value;
+    dataFiltered = filterByField(elementsSearched, field);
     updateCardsContainer(cardsContainer, dataFiltered);
   });
   
--- src/utils/functions.js
+++ src/utils/functions.js
@@ -3,3 +3,27 @@ export const getFields = (data) => {
   const uniqueMainFields = [...new Set(mainFields)];
   return uniqueMainFields;
 }
+
+export const filterByField = (data, field) => {
+  let dataFiltered = [...data];
+  if (field) {
+    return dataFiltered = data.filter(element => element.facts.mainField === field);
+  } 
+  return  dataFiltered;
+};
+
+export const sortData = (data, sortValue) => {
+  const dataSorted = [...data];
+  if (sortValue === 'asc') {
+    return dataSorted.sort((a, b) => a.name.localeCompare(b.name));
+  } else if (sortValue === 'desc') {
+    return dataSorted.sort((a, b) => b.name.localeCompare(a.name));
+  }
+  return dataSorted;
+}
+
+export const findElements = (data, userInput) => {
+  console.log('data', data)
+  const dataSearched = data.filter(element => element.name.toLowerCase().includes(userInput.toLowerCase()));
+  return dataSearched;
+}
